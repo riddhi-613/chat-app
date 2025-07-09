85739b05bf5ac041ff898b3837dd3e1ae8578315
Riddhi Sekhri <riddhisekhri1334@gmail.com>
2025-07-09 19:48:49 +0530

    Update Signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 
 const app=express();
 
-app.use("api/auth", authRoutes)
+const PORT=process.env.PORT;
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001")
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port PORT:"+PORT);
+    connectDB();
 });
